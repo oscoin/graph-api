593c48aecaa1e526f8471a7af09277243ad388a4
Alexis Sellier <alexis@monadic.xyz>
2019-09-16 10:42:16 +0200

    Make some small changes to

--- src/types.rs
+++ src/types.rs
@@ -26,9 +26,10 @@ pub enum EdgeType {
 pub struct EdgeData {
     /// The type of edge.
     edge_type: EdgeType,
-    /// A strength factor for this edge. Eg. the number of contributions
-    /// on a contribution edge.
-    strength: f64,
+    /// The weight of this specific edge. Can be used to weight for eg.
+    /// edges with more contributions higher, or weigh certain dependencies
+    /// higher than others. Defaults to `1.0`.
+    weight: f64,
 }
 
 /// The rank or "osrank" of a node, normalized to `1.0`.
@@ -36,7 +37,7 @@ pub type NodeRank = f64;
 
 /// Global parameters used by the graph algorithm.
 pub struct HyperParameters {
-    /// Threshold below which nodes are pruned in the first
+    /// Also `tau`. Threshold below which nodes are pruned in the first
     /// phase of the algorithm.
     pruning_threshold: f64,
     /// Probability that a random walk on a project node continues.
